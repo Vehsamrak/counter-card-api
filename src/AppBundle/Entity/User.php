09fc187450478c5c7ed139345c4c191e23414590
Petr Karmashev <smonkl@bk.ru>
2017-06-20 08:32:56 +0300

    User is confirmed by default

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -118,7 +118,7 @@ class User implements UserInterface
         $this->password = md5($password);
         $this->ip = $userIp;
         $this->cards = new ArrayCollection();
-        $this->isConfirmed = false;
+        $this->isConfirmed = true;
     }
 
     public function getId(): string
