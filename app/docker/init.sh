0123c6da49af30a830165cf9af9d4a7df49a72cf
Petr Karmashev <smonkl@bk.ru>
2017-06-25 23:23:51 +0300

    Init.sh for docker endpoint

--- app/docker/init.sh
+++ app/docker/init.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
+echo "Setting parameters ..."
+if [ ! -f /project/app/config/parameters.yml ]; then
+    echo "Parameters are not found! Copying from .dist"
+    cp /project/app/config/parameters.yml.dist /project/app/config/parameters.yml
+fi
+
+echo "Starting PHP-fpm ..."
+service php7.1-fpm start
+
+echo "Installing composer dependencies ..."
+cd /project
+/composer.phar install
+
+/project/bin/console --env=prod ca:cl
+/project/bin/console --env=dev ca:cl
+/project/bin/console --env=test ca:cl
+
+echo "Creating databases ..."
+/project/bin/console --env=prod doc:dat:cr
+/project/bin/console --env=dev doc:dat:cr
+
+echo "Database migration ..."
+/project/bin/console --env=prod doc:mig:mig -n
+/project/bin/console --env=dev doc:mig:mig -n
+
+echo "Clearing symfony cache ..."
+/project/bin/console --env=prod ca:cl
+
+echo "Starting Nginx ..."
 nginx -g 'daemon off;'
