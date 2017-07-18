223f12b469b2acb11803c27ff1d65d4384ad416c
Petr Karmashev <smonkl@bk.ru>
2017-07-18 23:01:34 +0300

    Simple git deploy scheme script

---
+++ app/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Changing to project directory ..."
+cd ..
+
+echo "Updating code from remote repository ..."
+git stash
+git pull
+git stash pop
+
+echo "Installing composer dependencies ..."
+~/composer.phar install
+
+bin/console --env=prod ca:cl
+
+echo "Database migration ..."
+bin/console --env=prod doc:mig:mig -n
+
+echo "Clearing symfony cache ..."
+bin/console --env=prod ca:cl
